97fb95a088b4b920a148b4071d9fba5cd3fe487c
Aditi Shah <119335339+aditishah24@users.noreply.github.com>
2023-07-25 18:51:31 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,58 @@
+import React,{useState} from "react";
+import Header from "./Components/Header/Header";
+import UserInput from "./Components/UserInput/UserInput";
+import ResultsTable from "./Components/Resultstable/ResultsTable";
+
+function App() {
+
+  const[userInput,setUserInput] = useState(null);
+
+  const calculateHandler = (userInput) => {
+    setUserInput(userInput);
+    
+};
+
+    // do something with yearlyData ...
+    // Should be triggered when form is submitted
+    // You might not directly want to bind it to the submit event on the form though...
+
+    const yearlyData = []; // per-year results
+
+    if(userInput){
+    let currentSavings = userInput['current-savings']; // feel free to change the shape of this input object!
+    const yearlyContribution = userInput['yearly-contribution']; // as mentioned: feel free to change the shape...
+    const expectedReturn = userInput['expected-return'] / 100;
+    const duration = userInput['duration'];
+
+    // The below code calculates yearly results (total savings, interest etc)
+    for (let i = 0; i < duration; i++) {
+      const yearlyInterest = currentSavings * expectedReturn;
+      currentSavings += yearlyInterest + yearlyContribution;
+      yearlyData.push({
+        // feel free to change the shape of the data pushed to the array!
+        year: i + 1,
+        yearlyInterest: yearlyInterest,
+        savingsEndOfYear: currentSavings,
+        yearlyContribution: yearlyContribution,
+      });
+    
+  };
+}
+
+    
+
+  return (
+    <div>
+      <Header />
+        
+
+      <UserInput onCalculate={calculateHandler}/>
+      {!userInput && <p style={{textAlign:'center'}}> No Investment Calculated</p>}
+      {/* Todo: Show below table conditionally (only once result data is available) */}
+      {/* Show fallback text if no data is available */}
+      {userInput && <ResultsTable data={yearlyData} initialInvestment={userInput['current-savings']}/>}
+    </div>
+  );
+}
+
+export default App;
